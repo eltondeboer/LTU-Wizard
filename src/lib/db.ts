f62a1cb526303591261452819ec0cd839e9f39f0
eltondeboer <62113726+eltondeboer@users.noreply.github.com>
2024-11-02 15:26:10 +0800

    added .env

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,14 @@
 import mysql from 'mysql2/promise'
 
+if (!process.env.DB_HOST) throw new Error('DB_HOST is not defined')
+if (!process.env.DB_USER) throw new Error('DB_USER is not defined')
+if (!process.env.DB_PASSWORD) throw new Error('DB_PASSWORD is not defined')
+if (!process.env.DB_NAME) throw new Error('DB_NAME is not defined')
+
 export const db = mysql.createPool({
-  host: 'ltuwiz.cn6kgi0si6vw.eu-north-1.rds.amazonaws.com',
-  port: 3306,
-  user: 'admin',
-  password: 'ltuwizard123',
-  database: 'system'
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '3306'),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 }) 
